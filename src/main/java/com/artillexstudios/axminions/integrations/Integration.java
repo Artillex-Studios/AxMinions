102c4ec5a127d3a17aeca3753c151d0743c397cd
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-07-31 18:34:15 +0200

    Add prioritised registration

--- src/main/java/com/artillexstudios/axminions/integrations/Integration.java
+++ src/main/java/com/artillexstudios/axminions/integrations/Integration.java
@@ -1,6 +1,7 @@
 package com.artillexstudios.axminions.integrations;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.joml.Math;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,6 +12,10 @@ public abstract class Integration<T extends Integrable> {
 
     public void register(T integration) {
         this.integrations.add(integration);
+    }
+
+    public void register(int priority, T integration) {
+        this.integrations.add(Math.clamp(0, integrations.size(), priority), integration);
     }
 
     public List<T> integrations() {
